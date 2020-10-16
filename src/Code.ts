1ef41b4ed501786d7dd46fc6685f91b41fe5a33c
JeanRemiDelteil <jeanremi.delteil@gmail.com>
2020-10-16 14:14:08 +0200

    generalize onEdit calls

--- src/Code.ts
+++ src/Code.ts
@@ -1,14 +1,41 @@
 import { onPersoClassChange } from './perso';
 
-const PersoClassColumn = 2;
+
+interface EditActionMap {
+	[sheetName: string]: {
+		[columnIndex: string]: (value: string, range: GoogleAppsScript.Spreadsheet.Range) => void;
+	};
+}
+
+enum SheetName {
+	CharList = 'Personnages',
+}
+
 
 /**
- * Called on every edit in the Spreadsheet
+ * Called on every edit in the Spreadsheet,
+ * If edit is on a range of more than 1 cell, value is undefined
  */
-export function onEdit(event: { value: string, range: GoogleAppsScript.Spreadsheet.Range }): void {
-	// Select eventType
-	if (event.range.getColumn() === PersoClassColumn) {
-		onPersoClassChange(event.value, event.range);
-	}
+export function onEdit(event: { value?: string, range: GoogleAppsScript.Spreadsheet.Range }): void {
+
+	const onEditActionMap: EditActionMap = {
+		[SheetName.CharList]: {
+			'2': onPersoClassChange,
+		},
+	};
+	
+
+	const sheetName = event.range
+		.getSheet()
+		.getSheetName();
+	const columnIndex1 = event.range.getColumn();
+
+	const sheetActions = onEditActionMap[sheetName];
+	if (!sheetActions) return;
+
+	const columnAction = sheetActions[columnIndex1];
+	if (!columnAction) return;
+
+	columnAction(event.value, event.range);
 }
 
